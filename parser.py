c9fdd9164b693f9c6c1a11dc626912a2dde29006
Mahmoud Rashed <37276859+MahmoudRashed2505@users.noreply.github.com>
2020-07-09 20:49:53 +0200

    finalize

--- parser.py
+++ parser.py
@@ -1,12 +1,42 @@
 import feedparser
+from datetime import datetime
+import os
+import shutil
+import smtplib
+
+gm = os.environ.get('gm')
+gpassword = os.environ.get('gpassword')
 
 print()
 with open('url.txt') as file:
     urls = file.readlines()
+    os.mkdir('RSS '+str(datetime.date(datetime.now())))
+    os.chdir('RSS '+str(datetime.date(datetime.now())))
     for url in urls:
         feed = feedparser.parse(url)
-        print()
-        print(url)
-        print()
+        site = url.split('.')[1]+'.txt'
+        print(site)
+        files = os.listdir()
+        if site not in files:
+            f = open(site,'w')
+        else:
+            f = open(site,'a')
         for entry in feed.entries:
-            print("Title: {}\nPublished Date: {}\nLink: {}\n".format(entry['title'],entry['published'],entry['link']))
+            f.write("Title: {}\nLink: {}\n\n".format(entry['title'],entry['link']))
+        f.close()
+
+os.chdir('..')
+shutil.move('RSS '+str(datetime.date(datetime.now())),"C:\\Users\\MahmoudHossamEldenIb\\Desktop")
+
+with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(gm,gpassword)
+    subject = "New RSS Available on Desktop"
+    body = "Hey, Check your Desktop for the New RSS"
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(gm,"mahmoud.rashed2505@outlook.com",msg)
